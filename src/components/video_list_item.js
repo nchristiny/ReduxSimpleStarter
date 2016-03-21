57f6bde82827beb43dba728ed327179d68873032
Nick Christiny <nchristiny@jellyvision.com>
2016-03-21 12:01:40 -0500

    Finish section 2

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
-  // const video = props.video;
+const VideoListItem = ({video, onVideoSelect}) => {
+  // const video = props.video; is equivalent to:
   // instead of calling props above on line 3, the shorthand above is ES6,
   // {video} is identical to const video = props.video
   const imageURL = video.snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageURL} />
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -12,21 +13,33 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-      // Shorthand for
-      // this.setState({ videos: videos });
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      // this.setState({videos}) is shorthand for
+      // this.setState({videos: videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
